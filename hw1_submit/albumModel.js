d3acf18626856541ef8fe89f97dc8b26eb9431f2
AyimenH <92697011+AyimenH@users.noreply.github.com>
2025-10-09 02:43:58 +0900

    Delete hw1_submit directory

--- hw1_submit/albumModel.js
+++
@@ -1,8 +0,0 @@
-const albums = [
-    { id: 1, name: "Ride The Lightning", year: 1984, genre: "Heavy Metal", tracks: [
-            { trackNumber: 9, title: "For Whom The Bell Tolls", duration: "5:11", primaryArtist: "Metallica" },
-        ]
-    },
-]
-
-module.exports = albums
--- hw1_submit/concertModel.js
+++
@@ -1,5 +0,0 @@
-const concerts = [
-    { id: 0, date: "2022-08-09", primaryArtist: "Metallica" },
-]
-
-module.exports = concerts
